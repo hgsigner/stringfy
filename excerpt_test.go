4890b6e53f5f860678933017c8eb9aa343001f3f
Hugo Miranda Mac <hugo@aioninnovations.com>
2015-10-14 15:59:16 -0700

    add benchmark for excerpt perform function

--- excerpt_test.go
+++ excerpt_test.go
@@ -6,11 +6,11 @@ import (
 	"github.com/hgsigner/stringfy"
 )
 
-func Test_Excerpt(t *testing.T) {
-
-	text := `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+var text string = `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
 
 The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`
+
+func Test_Excerpt(t *testing.T) {
 
 	tests := []struct {
 		text         string
@@ -108,5 +108,13 @@ The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for t
 		if excf != test.out {
 			t.Errorf("\nExpected: %s\nGot:      %s", test.out, excf)
 		}
+	}
+}
+
+func BenchmarkExcerptPerform(b *testing.B) {
+	exc := stringfy.NewExcerpt()
+	exc.Options(stringfy.AddRadious(1), stringfy.AddSeparator(" "))
+	for i := 0; i < b.N; i++ {
+		exc.Perform(text, "popular")
 	}
 }
